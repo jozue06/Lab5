4cf99f964d4565ec1bdba287f3375df43e1bb479
Jozue06 <jozue06@gmail.com>
2018-01-20 14:07:50 -0800

    fixed multiply

--- app.js
+++ app.js
@@ -8,28 +8,26 @@ Write a function called sum() that takes in two numbers as arguments and then re
 "The sum of 4 and 7 is 11."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-
 function sum(a, b) {
   var sums = (a + b);
-  var sumArray = [];
-  sumArray.push([sums],['The sum of ' + a + ' and ' + b + ' = ' + sums + '.']);
-  console.log ('The sum of ' + a + ' and ' + b + ' = ' + sums + '.');
-  console.log (sumArray);
+  var array1 = [sums,'The sum of ' + a + ' and ' + b + ' is ' + sums + '.'];
+  return array1;
 }
+
 
 //Here is the test for sum(); uncomment it to run it
 testSum(4,7);
 
 function multiply(a, b) {
   var product = (a * b);
-  var multArray = [];
-  multArray.push([product],['The product of ' + a + ' and ' + b + ' is ' + product + '.']);
+  var multArray = [product,'The product of ' + a + ' and ' + b + ' is ' + product + '.'];
   console.log ('The product of ' + a + ' and ' + b + ' is ' + product + '.');
   console.log (multArray);
+  return multArray;
 }
 
 //Here is the test for multiply(); uncomment it to run it
-multiply(5, 9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 

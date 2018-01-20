2649ffecec732a149ce900376cabd9ece432b8c0
Jozue06 <jozue06@gmail.com>
2018-01-20 14:20:35 -0800

    fixed question 3

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 "use strict";
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
+
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+
+
 "The sum of 4 and 7 is 11."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
 function sum(a, b) {
-  //eslint-disable-line
   var sums = (a + b);
-  var array = [sums,'The sum of ' + a + ' and ' + b + ' is ' + sums + '.'];
-  return array;
+  var array1 = [sums,'The sum of ' + a + ' and ' + b + ' is ' + sums + '.'];
+  return array1;
 }
 
-// Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+
+//Here is the test for sum(); uncomment it to run it
+testSum(4,7);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -24,15 +27,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
 function multiply(a, b) {
-  //e
+  var product = (a * b);
+  var multArray = [product,'The product of ' + a + ' and ' + b + ' is ' + product + '.'];
+  console.log ('The product of ' + a + ' and ' + b + ' is ' + product + '.');
+  console.log (multArray);
+  return multArray;
 }
 
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+//Here is the test for multiply(); uncomment it to run it
+testMultiply(5, 9);
 
 /////////////////////////////////////
 /* Problem 3
@@ -45,10 +49,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
+  var product = (a * b * c);
+  var sums = (a + b + c);
+  var sumMultArray = [sums, product, + a + ' and ' + b + ' and ' + c + ' sum to ' + sums + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product + '.'];
+  return sumMultArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 

84186263480f5a2648e786b034ddde44894b6fae
Jozue06 <jozue06@gmail.com>
2018-01-20 13:40:19 -0800

    going back to master to fix tests

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function sum(a, b) {
 }
 
 //Here is the test for sum(); uncomment it to run it
-sum(4, 7);
+testSum(4,7);
 
 function multiply(a, b) {
   var product = (a * b);
@@ -28,7 +28,7 @@ function multiply(a, b) {
   console.log (multArray);
 }
 
-//Here is the test for sum(); uncomment it to run it
+//Here is the test for multiply(); uncomment it to run it
 multiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -39,15 +39,6 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
-//function multiply(a, b) {
-  //eslint-disable-line
-//}
-
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
